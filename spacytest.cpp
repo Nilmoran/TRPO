2d998cc22c5189072dc176a28712db089e03571b
Nilmoran <praimus2002@yandex.ru>
2023-12-05 21:10:38 +0300

    Co-authored-by: FoxInNight1337 <FoxInNight1337@users.noreply.github.com>

--- spacytest.cpp
+++ spacytest.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <filesystem>
 #include <map>
+#include <sstream>
+#include <cctype>
+#include <regex>
 
 using namespace std;
 
@@ -12,33 +15,47 @@ namespace fs = std::filesystem;
 bool hasUnsavedChanges = false;
 bool uploadFromFile = false;
 
-struct WordInfo {
-    std::string word;
+struct Word {
+    string word;
+    string sentence;
     int count;
 
-    WordInfo() : count(0) {}
+    Word(const string& w, const string& s) : word(w), sentence(s), count(1) {}
 };
 
-void printMenu()
-{
+void printMenu();
+string createNumberedBufferFile(std::string&);
+string checkingFileName(int);
+bool isNounOrPron(string);
+void spacy(vector<Word>& );
+void printResults(const vector<Word>& );
+bool saveToFile(vector<Word>&, std::string&);
+bool exitSave(vector<Word>& );
+
+void printMenu() {
     cout << "Выберите операцию: " << endl
-        << "1: загрузить текст из файла" << endl
-        << "2: сохранить результаты в файл" << endl
-        << "3: вывести результат на консоль" << endl
-        << "4: выйти" << endl;
+         << "1: загрузить текст из файла" << endl
+         << "2: сохранить результаты в файл" << endl
+         << "3: вывести результат на консоль" << endl
+         << "4: выйти" << endl;
 }
 
-// Функция, которая отвечает за проверку названия файла на запрещённые символы
-// Также функция выполняет проверку на наличие в папке файл с таким же именем
-// Возвращает корректное название файла
-string checkingFileName(int mode)
-{
+// Функция для поиска слова в массиве words
+int findWordIndex(const vector<Word>& words, const string& word) {
+    for (size_t i = 0; i < words.size(); ++i) {
+        if (words[i].word == word) {
+            return static_cast<int>(i);
+        }
+    }
+    return -1;
+}
+
+string checkingFileName(int mode) {
     string path;
     ifstream buf;
     string fileName;
     bool flag;
-    do
-    {
+    do {
         cin.ignore(1000, '\n');
         cin.clear();
         flag = true;
@@ -48,14 +65,12 @@ string checkingFileName(int mode)
         else
             cout << "Введите символ ‘=’, для того чтобы прекратить ввод названия выходного файла и вернуться в главное меню." << endl;
 
-        getline(cin, fileName); // Получить название файла
-        if (fileName == "=")
-        {
+        getline(cin, fileName);
+        if (fileName == "=") {
             return fileName;
         }
 
-        for (int i = 0; i < fileName.size(); i++) // Проверить название файла на запрещенные символы
-        {
+        for (int i = 0; i < fileName.size(); i++) {
             if ((fileName[i] == '\\') ||
                 (fileName[i] == '/') ||
                 (fileName[i] == ':') ||
@@ -63,63 +78,47 @@ string checkingFileName(int mode)
                 (fileName[i] == '?') ||
                 (fileName[i] == '"') ||
                 (fileName[i] == '<') ||
-                (fileName[i] == '|')
-                )
-            {
+                (fileName[i] == '|')) {
                 cout << "Не корректный ввод, повторите попытку, не используя специальные символы!" << '\n';
                 flag = false;
                 break;
             }
         }
 
-        if (flag == true)
-        {
-            if (mode == 1) // Если это название входного файла, то проверить его наличие в папке с программой
-
-            {
-                if (fileName.find(".txt") == string::npos) // Добавить расширение, если его нет
-                {
+        if (flag == true) {
+            if (mode == 1) {
+                if (fileName.find(".txt") == string::npos) {
                     fileName += ".txt";
                 }
                 path = fs::current_path().string() + "/" + fileName;
                 buf.open(path);
-                if (!buf.is_open()) // Если файл не удалось открыть
-                {
+                if (!buf.is_open()) {
                     cout << "Файл не найден проверьте его наличие по пути: " << path << endl;
                     flag = false;
                 }
             }
-            if (mode == 2) // Если это название выходного файла, то проверить на существование файла с таким же названием
-            {
-                if (fileName.find(".txt") == string::npos)
-                {
+            if (mode == 2) {
+                if (fileName.find(".txt") == string::npos) {
                     fileName += ".txt";
                 }
-                if (fs::exists(fileName)) // Если файл существует, то добавить к нему номер
-                {
+                if (fs::exists(fileName)) {
                     int count = 1;
                     string baseName, extension, newFileName;
-                    size_t dotPos = fileName.find_last_of('.'); // Определить позицию с которой начинается расширение
-
-                    // Разделить полученное на название файла и его расширение
-                    if (dotPos != string::npos)
-                    {
+                    size_t dotPos = fileName.find_last_of('.');
+                    if (dotPos != string::npos) {
                         baseName = fileName.substr(0, dotPos);
                         extension = fileName.substr(dotPos);
                     }
 
-                    do
-                    {
-                        newFileName = baseName + "(" + to_string(count) + ")" + extension;  // Добавить номер между названием файла и его расширением
+                    do {
+                        newFileName = baseName + "(" + to_string(count) + ")" + extension;
                         count++;
                     } while (fs::exists(newFileName));
                     path = fs::current_path().string() + "/" + newFileName;
                     cout << "Файл с таким названием уже существует, поэтому будет создан файл:\n " << path << endl;
 
                     return newFileName;
-                }
-                else
-                {
+                } else {
                     path = fs::current_path().string() + "/" + fileName;
                     cout << "Текущая структура данных была записана в файл по пути " << path << endl;
                     return fileName;
@@ -128,26 +127,23 @@ string checkingFileName(int mode)
         }
 
     } while (flag == false);
-
     return fileName;
 }
 
-bool isNounOrPron(string tokenPos) { // Функция проверяющая является ли слово существительным или местоимением
+bool isNounOrPron(string tokenPos) {
     if (tokenPos == "NOUN" || tokenPos == "PRON" || tokenPos == "PROPN") {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
 
-void spacy(std::map<std::string, WordInfo>& wordMap)
-{
+void spacy(vector<Word>& words) {
     string str, text;
     string fileName;
-    fileName = checkingFileName(1); // Получить название входного файла
-    if (fileName == "=")
-    {
+    int count = 0;
+    fileName = checkingFileName(1);
+    if (fileName == "=") {
         return;
     }
     ifstream input(fileName);
@@ -158,68 +154,78 @@ void spacy(std::map<std::string, WordInfo>& wordMap)
         cout << "Файл успешно открыт!" << endl;
         uploadFromFile = true;
 
-        while (getline(input, str))
-        {
+        while (getline(input, str)) {
             auto doc = nlp.parse(str);
-
-            for (auto& token : doc.tokens())
+            for (auto& sent : doc.sents()) 
             {
-                auto head = token.head();
-                auto children = token.children();
-                if (isNounOrPron(token.pos_()) &&
+                for (auto& token : doc.tokens()) 
+                {
+                    auto head = token.head();
+                    if (isNounOrPron(token.pos_()) &&
                     (token.dep_() == "obl" || token.dep_() == "obj" || token.dep_() == "nmod" || token.dep_() == "iobj" || token.dep_() == "conj") &&
                     !(token.dep_() == "obj" && head.dep_() == "xcomp") && !(token.dep_() == "obl" && head.dep_() == "xcomp") &&
                     !(token.dep_() == "obl" && head.dep_() == "acl") && !(token.dep_() == "obl" && head.dep_() == "advcl") &&
                     !(token.dep_() == "conj" && head.dep_() == "obl") && !(token.dep_() == "obl" && head.dep_() == "ROOT") &&
                     !(token.dep_() == "conj" && head.dep_() == "nsubj") && !(token.dep_() == "conj" && head.dep_() == "obl") &&
                     !(token.dep_() == "conj" && head.dep_() == "obj")) 
-                {
-                    std::string word = token.text();
-                    flagObjectiv = true;
-                    // Поиск слова в map и обновление счетчика
-                    wordMap[word].count++;
+
+                    {
+                        std::string word = token.text();
+                        std::string sentence = sent.text();
+                        int index = findWordIndex(words, word);
+                        if (index != -1) {
+                            // Слово уже встречалось, увеличить счетчик и обновить предложение, если оно новое
+                            cout << index << endl;
+                            words[index].count++;
+                            if (sentence != words[index].sentence) {
+                                words[index].sentence = sentence;
+                            }
+                            words.push_back(Word(word, sentence));
+                        } else {
+                            // Слово встречается впервые, добавить его в массив
+                            cout << index << endl;
+                            words.push_back(Word(word, sentence));
+                        }
+                        flagObjectiv = true;
+                    }
                 }
             }
         }
     }
-    if (flagObjectiv == true)
-    {
+    if (flagObjectiv == true) {
         cout << "Дополнения обнаружены в файле!" << endl;
         hasUnsavedChanges = true;
-    }
-    else
-    {
+    } else {
         cout << "Дополнения не обнаружены в файле!" << endl;
     }
 
     input.close();
 }
 
-void printWordInfo(const std::map<std::string, WordInfo>& wordMap)
-{
-    cout << "Слова и их количество:" << endl;
-
-    for (const auto& pair : wordMap)
-    {
-        cout << pair.first << ": " << pair.second.count << endl;
+// Функция для вывода результатов
+void printResults(const vector<Word>& words) {
+    for (const auto& word : words) {
+        cout << "Слово: " << word.word << endl;
+        cout << "Предложение: " << word.sentence << endl;
+        cout << "Количество: " << word.count << endl;
+        cout << "---------------------" << endl;
     }
     cin.get();
 }
 
-// Функция, которая отвечает за сохранение данных из структуры во внешний текстовый файл
-bool saveToFile(const std::map<std::string, WordInfo>& wordMap, const std::string& fileName)
-{
+bool saveToFile(vector<Word>& words, std::string& fileName) {
     system("clear");
     ofstream output(fileName);
-    if (!output.is_open())
-    {
+    if (!output.is_open()) {
         cout << "Ошибка при открытии файла для записи." << endl;
         return false;
     }
 
-    for (const auto& pair : wordMap)
-    {
-        output << pair.first << ": " << pair.second.count << endl;
+    for (const auto& word : words) {
+        cout << "Слово: " << word.word << endl;
+        cout << "Предложение: " << word.sentence << endl;
+        cout << "Количество: " << word.count << endl;
+        cout << "---------------------" << endl;
     }
 
     cout << "Результаты сохранены в файл: " << fileName << endl;
@@ -227,120 +233,91 @@ bool saveToFile(const std::map<std::string, WordInfo>& wordMap, const std::strin
     return true;
 }
 
-// Функция, которая отвечает за сохранение данных перед выходом из функции
-bool exitSave(const std::map<std::string, WordInfo>& wordMap)
-{
+bool exitSave(vector<Word>& words) {
     bool validInput = false;
     char input;
     cout << "Хотите сохранить список книг перед выходом? (y/n)" << endl;
     cout << "Или введите символ ‘=’, для того чтобы вернуться в главное меню. " << endl;
 
-    while (!validInput)
-    {
-        input = getchar(); // Получить выбор пользователя
-
-        if (input == 'y' || input == 'Y')
-        {
+    while (!validInput) {
+        input = getchar();
+        if (input == 'y' || input == 'Y') {
             string saveFileName = checkingFileName(2);
-            if(saveFileName == "=")
-            {
-                return 0;
+            if (saveFileName == "=") {
+                return false;
             }
-            bool exit = saveToFile(wordMap, saveFileName);
-            if (exit == 0)
-            {
-                return 0;
+            bool exit = saveToFile(words, saveFileName);
+            if (!exit) {
+                return false;
             }
             break;
-        }
-        else if (input == 'n' || input == 'N')
-        {
+        } else if (input == 'n' || input == 'N') {
             break;
-        }
-        else if (input == '=')
-        {
-            return 0;
+        } else if (input == '=') {
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
-int main()
-{
-    std::map<std::string, WordInfo> wordMap;
-    // Цикл меню
+int main() {
+    vector<Word> words;
     bool validInput = false;
     char input;
     system("clear");
-    printMenu(); // Вывод главного меню
+    printMenu();
 
-    while (!validInput)
-    {
+    while (!validInput) {
         input = getchar();
-        if (input == '1')
-        {
-            spacy(wordMap); // Вызвать функцию парсинга текста
+        if (input == '1') {
+            spacy(words);
             cout << "Нажмите клавишу Enter для продолжения." << endl;
             cin.get();
             system("clear");
             printMenu();
             continue;
         }
-        if (input == '2') 
-        {
+        if (input == '2') {
             string saveFileName = checkingFileName(2);
-            if(saveFileName == "=")
-            {
+            if (saveFileName == "=") {
                 system("clear");
                 printMenu();
                 continue;
             }
-            
-            if (saveToFile(wordMap, saveFileName)) // Вызвать функцию вывода найденных слов в файл
-            {
+
+            if (saveToFile(words, saveFileName)) {
                 hasUnsavedChanges = false;
             }
-            cout << "Нажмите клавишу Enter для продолжения." << endl;
-            cin.get(); 
-            system("clear");
-            printMenu();
-            continue;
-        }
-        if (input == '3') 
-        {
-            printWordInfo(wordMap); // Вывод найденных слов в консоль
             cout << "Нажмите клавишу Enter для продолжения." << endl;
             cin.get();
             system("clear");
             printMenu();
             continue;
         }
-        if (input == '4') // Выход их программы 
-        {
-            if (uploadFromFile == false)
-            {
+        if (input == '3') {
+            printResults(words);
+            cout << "Нажмите клавишу Enter для продолжения." << endl;
+            cin.get();
+            system("clear");
+            printMenu();
+            continue;
+        }
+        if (input == '4') {
+            if (uploadFromFile == false) {
                 break;
-            }
-            else
-            {
-                if (hasUnsavedChanges == true) // Если есть несохранные изменения в программе 
-                {
+            } else {
+                if (hasUnsavedChanges == true) {
                     system("clear");
-                    bool exit = exitSave(wordMap); // Сохранить список слов перед выходом из программы
-                    if (exit)
-                    {
+                    bool exit = exitSave(words);
+                    if (exit) {
                         validInput = true;
                         break;
-                    }
-                    else
-                    {
+                    } else {
                         system("clear");
                         printMenu();
                         continue;
                     }
-                }
-                else
-                {
+                } else {
                     validInput = true;
                     break;
                 }
